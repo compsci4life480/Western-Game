18f48c1c2bffb8026313935043a5515c8cef83bc
compsci4life480 <eriko480@Gmail.com>
2021-10-17 13:27:26 -0600

    Update Timer.cs

--- Western Gun Course - Game 2/Assets/Scripts/Timer.cs
+++ Western Gun Course - Game 2/Assets/Scripts/Timer.cs
@@ -7,20 +7,19 @@ public class Timer : MonoBehaviour
 {
 
     public Text timerText;
-    public float startTime;
-    // Start is called before the first frame update
+    //Needs to set to 65 to start at 60 for some reason??? 
+    float timeRemaining;
+
     void Start()
     {
-        startTime = Time.time;
+        timeRemaining = 60f;
     }
-
     // Update is called once per frame
     void Update()
     {
-        float t = Time.time - startTime;
-        string minutes = ((int)t / 60).ToString();
-        string seconds = (t % 60).ToString("f2");
-        
-        timerText.text = minutes + ":" + seconds;
+        //Debug.Log(Time.unscaledDeltaTime);
+        timeRemaining -= Time.unscaledDeltaTime;
+        string seconds = (timeRemaining % 60).ToString("f2");
+        timerText.text = seconds;
     }
 }
