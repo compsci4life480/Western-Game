ce0ee8a9d4c574ea9c5352e654e42ca5a339a1e3
compsci4life480 <eriko480@Gmail.com>
2021-10-18 10:01:12 -0600

    Balls Go In Direction of Player (Updated)

--- Western Gun Course - Game 2/Assets/Scripts/BallForce.cs
+++ Western Gun Course - Game 2/Assets/Scripts/BallForce.cs
@@ -8,13 +8,19 @@ public class BallForce : MonoBehaviour
     private Vector3 dir;
     public float speed;
     public float velocity;
+    Vector3 targetPosition;
     Rigidbody rb;
     
     // Start is called before the first frame update
     void Start()
     {
-        dir = target.transform.position - transform.position;
-       // dir = dir.normalized;
+       // dir = target.transform.localPosition - transform.position;
+        targetPosition = GameObject.Find("First Person Player").transform.position;
+        dir = targetPosition - transform.position;
+        dir = dir.normalized;
+        //Debug.Log(dir);
+        //Debug.Log("target" + target.transform.localPosition + "This" + transform.position + " " + dir);
+        // dir = dir.normalized;
         rb = gameObject.GetComponent<Rigidbody>();
         //rb.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * speed);
 
@@ -25,6 +31,6 @@ public class BallForce : MonoBehaviour
     {
         //dir = target.transform.position - transform.position;
         rb.velocity *= velocity;
-        rb.AddForce(dir.normalized * speed);
+        rb.AddForce(dir * speed);
     }
 }
